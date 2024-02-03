8a66168aa747109b2287342d0bcd82f90a6a3c19
tssmitha <smithats133@gmail.com>
2024-02-04 00:46:10 +0530

    seventh commit

--- src/Pages/RegisterPage/Register.jsx
+++ src/Pages/RegisterPage/Register.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate} from 'react-router-dom';
 import axios from 'axios';
 import './RegisterPage.css'; // Import CSS file for styling
-import { Link } from 'react-router-dom';
 
 const Register = () => {
   const navigate = useNavigate();
